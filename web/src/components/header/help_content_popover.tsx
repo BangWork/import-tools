d4d05cb99138f3d5d2faa0ba343aa9f74b38e392
zhuhaolin <2869652294@qq.com>
2023-03-14 15:07:28 +0800

    feat "download file name"

--- web/src/components/header/help_content_popover.tsx
+++ web/src/components/header/help_content_popover.tsx
@@ -1,8 +1,9 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import type { FC } from 'react';
 import { map } from 'lodash-es';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router';
 
 import { downloadFile } from '@/utils/download';
 
@@ -27,37 +28,73 @@ const HelpContentPopover: FC<HelpContentPopoverProps> = memo((props) => {
   const { onSelected, label } = props;
   const { t } = useTranslation();
   const en = 'en';
-  const HELP_LIST = [
-    {
-      text: 'common.help.import',
-      onClick: () => {
-        if (label === en) {
-          downloadFile(
-            `${window.location.origin}/public/mappingEn.xlsx`,
-            'Jira import mapping form'
-          );
-        } else {
-          downloadFile(`${window.location.origin}/public/mappingZh.xlsx`, 'Jira数据导入清单');
-        }
-      },
-    },
-    {
-      text: 'common.help.document',
-      onClick: () => {
-        downloadFile(`${window.location.origin}/public/help.pdf`, 'help');
-      },
-    },
-    {
-      text: 'common.help.download',
-      onClick: () => {
-        downloadFile(
-          `${
-            import.meta.env.VITE_PROXY_DOMAIN_REAL || window.location.origin
-          }/import/log/download/all`
-        );
-      },
-    },
-  ];
+  let controlFlag = false;
+  const location = useLocation();
+  const HELP_LIST = useMemo(() => {
+    if (
+      location.pathname === '/page/analyze/environment' ||
+      location.pathname === '/page/analyze/pack'
+    ) {
+      controlFlag = false;
+    } else {
+      controlFlag = true;
+    }
+    if (controlFlag) {
+      return [
+        {
+          text: 'common.help.import',
+          onClick: () => {
+            if (label === en) {
+              downloadFile(
+                `${window.location.origin}/public/mappingEn.xlsx`,
+                'Jira import mapping form'
+              );
+            } else {
+              downloadFile(`${window.location.origin}/public/mappingZh.xlsx`, 'Jira数据导入清单');
+            }
+          },
+        },
+        {
+          text: 'common.help.document',
+          onClick: () => {
+            downloadFile(`${window.location.origin}/public/help.pdf`, 'help');
+          },
+        },
+        {
+          text: 'common.help.download',
+          onClick: () => {
+            downloadFile(
+              `${
+                import.meta.env.VITE_PROXY_DOMAIN_REAL || window.location.origin
+              }/import/log/download/all`
+            );
+          },
+        },
+      ];
+    } else {
+      return [
+        {
+          text: 'common.help.import',
+          onClick: () => {
+            if (label === en) {
+              downloadFile(
+                `${window.location.origin}/public/mappingEn.xlsx`,
+                'Jira import mapping form'
+              );
+            } else {
+              downloadFile(`${window.location.origin}/public/mappingZh.xlsx`, 'Jira数据导入清单');
+            }
+          },
+        },
+        {
+          text: 'common.help.document',
+          onClick: () => {
+            downloadFile(`${window.location.origin}/public/help.pdf`, 'help');
+          },
+        },
+      ];
+    }
+  }, [location.pathname]);
 
   return (
     <div onClick={onSelected}>
